2357d0a5cef1caaf0fae7deeecab1f126117eaa0
neetesh <neeteshlodhi9555@email.com>
2024-12-28 12:05:44 +0530

    githubApi added

--- index.js
+++ index.js
@@ -3,6 +3,47 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = 4000;
+const githubApi = 
+          {
+  "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
+
+
+
 
 app.get("/", (req, res) => {
           res.send("hello world");
@@ -13,6 +54,10 @@ app.get("/instagram", (req,res) => {
 })
 app.get("/login", (req,res) => {
          res.send("<button>login page</button>") 
+})
+
+app.get("/github", (req,res) => {
+          res.json(githubApi)
 })
 
 app.listen(process.env.PORT, () => {
